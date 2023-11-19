c5fc61d71699ed8c119b2701ee4f64d1d5273411
Edu <necholhite@gmail.com>
2023-11-19 13:58:46 -0300

    cont8inuação do projeto

--- temporizador/script.js
+++ temporizador/script.js
@@ -1,13 +1,24 @@
 const pomodoroMinutos = 0
-const pomodoroSegundos = 2
+const pomodoroSegundos = 5
 
 let minutos = pomodoroMinutos
 let segundos = pomodoroSegundos
 let ligado = false
 let interval
+let segtotal
+let deg
+const circularProgress = document.getElementById("timer")
 const minutosEl= document.getElementById("minutos")
 const segundosEl = document.getElementById("segundos")
+const pomodoroMax = 1500
+const pauseMax = 125
 
+function progressBar(){
+    segtotal = (minutos*60) + segundos
+    deg = (segtotal/pomodoroMax)* 360
+    console.log(deg)
+    circularProgress.style.background = `conic-gradient(cadetblue ${deg}deg, rgb(226, 226, 226) 0deg)`
+}
 
 function temporizador(){
     interval = setInterval(()=>{
@@ -16,13 +27,17 @@ function temporizador(){
             minutos -= 1
             segundos = 59
         }
+        if(segundos <= 0 && minutos<= 0){
+            resetar()
+        }
+        progressBar()
         minutosEl.innerHTML = minutos
         segundosEl.innerHTML = segundos
     },1000)
 }
 
 function controlador(){
-    const botao = document.querySelector('button#botao')
+    const botao = document.querySelector('button#iniciar')
     if (!ligado){
         ligado = true
         temporizador()
@@ -30,7 +45,7 @@ function controlador(){
     } else{
         clearInterval(interval)
         ligado = false
-        botao.innerHTML = "Iniciar"
+        botao.innerHTML = "Continuar"
     }
 }
 
